62b304baf53d8fd6b78dbbb09eeab0cecad6abd6
Tayná Martins Ramos <simples.tayna@gmail.com>
2020-08-01 03:06:52 -0300

    Adição de rotas e pages de cadastro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  {/* switch funciona como um if para as rotas */}
+  <Switch>
+    {/* as rotas sao as condições, ex: se o caminho for "/" renderizar o componente app */}
+    {/* obs: quanto mais especifico a rota, melhor ficar acima das outras, pois o switch vai testando as rotas nas ordem */}
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+
+    <Route exact path="/" component={Home} />
+    {/* para retornar uma pagina de erro 404 não precisa colocar path, ou seja, td q nao estiver noos paths das rotas existentes irá renderizar o 404 */}
+    {/* criar componente para 404 */}
+    <Route component={() => (<span>Página de erro 404</span>)} />
+
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
